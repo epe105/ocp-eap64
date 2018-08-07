dedd13ac37862426d5a0c2e5e42f3af66c859003
Mike Echevarria <mechevar@redhat.com>
2018-08-07 15:20:07 -0400

    remove project specific items

--- openshift-deploy.sh
+++ openshift-deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-project="$(oc projects | grep eap64)"
-
-if [[ -z ${project} ]]; then
-  oc new-project eap64 --display-name="EAP 6.4 WAR deploy" --description="Example project with EAP 6.4 deployed WAR using the dockerfile strategy"
-else
-  oc project eap64
-fi
-
 oc new-app https://github.com/mechevarria/ocp-eap64.git \
 --strategy=docker \
 --name=docker-eap64
