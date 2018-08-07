f48c45ad6f0832b44219e7eadd6a797b09c33859
Mike Echevarria <mechevar@redhat.com>
2018-08-07 15:03:33 -0400

    check for project creation

--- openshift-deploy.sh
+++ openshift-deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+project="$(oc projects | grep eap64)"
+
+if [[ -z ${project} ]]; then
+  oc new-project eap64 --display-name="EAP 6.4 WAR deploy" --description="Example project with EAP 6.4 deployed WAR using the dockerfile strategy"
+else
+  oc project eap64
+fi
 
 oc new-build https://github.com/mechevarria/ocp-eap64.git --strategy=docker --name=docker-eap64
